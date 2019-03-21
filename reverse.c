058c11ae40484f2dab0dd60bd939393d97e1f884
tasmiazerin <48786772+tasmiazerin@users.noreply.github.com>
2019-03-21 14:04:27 +0600

    Add files via upload

--- reverse.c
+++ reverse.c
@@ -4,15 +4,15 @@ int main()
   int a;
   printf("How many numbers:");
   scanf("%d",&a);
-  int b[a];
+  int c[a];
   for(int i=0;i<a;i++)
   {
   printf("Enter the number%d:",i+1);
-  scanf("%d",&b[i]);
+  scanf("%d",&c[i]);
   }
   for(int i=(a-1);i>=0;i--)
   {
-    printf("%d\n",b[i]);
+    printf("%d\n",c[i]);
   }
   return 0;
 }
